810dc528cf172d5d32bc2a5363210a8dd2d32267
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 10:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function shortcut(s1, s2) {
     // Check if either input string is empty
-  if (str1.length === 0 || str2.length === 0) {
+  if (s1.length === 0 || s2.length === 0) {
     return '';
   }
 
   // Extract the initial letters of the input strings and concatenate them
-  const initialLetters = str1.charAt(0) + str2.charAt(0);
+  const initialLetters = s1.charAt(0) + s2.charAt(0);
 
   return initialLetters;
 }
